0cd837aaca35c9c2e85b4848bc66d2b12d1a7643
s <65716060+sam-kuo@users.noreply.github.com>
2022-05-26 21:09:28 +0800

    Initialize issue-328

---
+++ contracts/adapters/Distribute.sol
@@ -0,0 +1,352 @@
+pragma solidity ^0.8.0;
+
+// SPDX-License-Identifier: MIT
+
+import "../core/DaoConstants.sol";
+import "../core/DaoRegistry.sol";
+import "../guards/MemberGuard.sol";
+import "../guards/AdapterGuard.sol";
+import "../adapters/interfaces/IVoting.sol";
+import "../adapters/interfaces/IDistribute.sol";
+import "../helpers/FairShareHelper.sol";
+import "../extensions/bank/Bank.sol";
+
+/**
+MIT License
+
+Copyright (c) 2020 Openlaw
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+contract DistributeContract is
+    IDistribute,
+    DaoConstants,
+    MemberGuard,
+    AdapterGuard
+{
+    // Event to indicate the distribution process has been completed
+    // if the unitHolder address is 0x0, then the amount were distributed to all members of the DAO.
+    event Distributed(address token, uint256 amount, address unitHolder);
+
+    // The distribution status
+    enum DistributionStatus {NOT_STARTED, IN_PROGRESS, DONE, FAILED}
+
+    // State of the guild kick proposal
+    struct Distribution {
+        // The distribution token in which the members should receive the funds. Must be supported by the DAO.
+        address token;
+        // The amount to distribute.
+        uint256 amount;
+        // The unit holder address that will receive the funds. If 0x0, the funds will be distributed to all members of the DAO.
+        address unitHolderAddr;
+        // The distribution status.
+        DistributionStatus status;
+        // Current iteration index to control the cached for-loop.
+        uint256 currentIndex;
+        // The block number in which the proposal has been created.
+        uint256 blockNumber;
+    }
+
+    // Keeps track of all the distributions executed per DAO.
+    mapping(address => mapping(bytes32 => Distribution)) public distributions;
+
+    // Keeps track of the latest ongoing distribution proposal per DAO to ensure only 1 proposal can be processed at a time.
+    mapping(address => bytes32) public ongoingDistributions;
+
+    /**
+     * @notice default fallback function to prevent from sending ether to the contract.
+     */
+    receive() external payable {
+        revert("fallback revert");
+    }
+
+    /**
+     * @notice Creates a distribution proposal for one or all members of the DAO, opens it for voting, and sponsors it.
+     * @dev Only tokens that are allowed by the Bank are accepted.
+     * @dev If the unitHolderAddr is 0x0, then the funds will be distributed to all members of the DAO.
+     * @dev Proposal ids can not be reused.
+     * @dev The amount must be greater than zero.
+     * @param dao The dao address.
+     * @param proposalId The guild kick proposal id.
+     * @param unitHolderAddr The member address that should receive the funds, if 0x0, the funds will be distributed to all members of the DAO.
+     * @param token The distribution token in which the members should receive the funds. Must be supported by the DAO.
+     * @param amount The amount to distribute.
+     * @param data Additional information related to the distribution proposal.
+     */
+    function submitProposal(
+        DaoRegistry dao,
+        bytes32 proposalId,
+        address unitHolderAddr,
+        address token,
+        uint256 amount,
+        bytes calldata data
+    ) external override reentrancyGuard(dao) {
+        IVoting votingContract = IVoting(dao.getAdapterAddress(VOTING));
+        address submittedBy =
+            votingContract.getSenderAddress(
+                dao,
+                address(this),
+                data,
+                msg.sender
+            );
+
+        require(amount > 0, "invalid amount");
+
+        _submitProposal(
+            dao,
+            proposalId,
+            unitHolderAddr,
+            token,
+            amount,
+            data,
+            submittedBy
+        );
+    }
+
+    /**
+     * @notice Creates the proposal, starts the voting process and sponsors the proposal.
+     * @dev If the unit holder address was provided in the params, the unit holder must have enough units to receive the funds.
+     */
+    function _submitProposal(
+        DaoRegistry dao,
+        bytes32 proposalId,
+        address unitHolderAddr,
+        address token,
+        uint256 amount,
+        bytes calldata data,
+        address submittedBy
+    ) internal onlyMember2(dao, submittedBy) {
+        // Creates the distribution proposal.
+        dao.submitProposal(proposalId);
+
+        BankExtension bank = BankExtension(dao.getExtensionAddress(BANK));
+        require(bank.isTokenAllowed(token), "token not allowed");
+
+        // Only check the number of units if there is a valid unit holder address.
+        if (unitHolderAddr != address(0x0)) {
+            // Gets the number of units of the member
+            uint256 units = bank.balanceOf(unitHolderAddr, UNITS);
+            // Checks if the member has enough units to reveice the funds.
+            require(units > 0, "not enough units");
+        }
+
+        // Saves the state of the proposal.
+        distributions[address(dao)][proposalId] = Distribution(
+            token,
+            amount,
+            unitHolderAddr,
+            DistributionStatus.NOT_STARTED,
+            0,
+            block.number
+        );
+
+        // Starts the voting process for the proposal.
+        IVoting votingContract = IVoting(dao.getAdapterAddress(VOTING));
+        votingContract.startNewVotingForProposal(dao, proposalId, data);
+
+        // Sponsors the proposal.
+        dao.sponsorProposal(proposalId, submittedBy, address(votingContract));
+    }
+
+    /**
+     * @notice Process the distribution proposal, calculates the fair amount of funds to distribute to the members based on the units holdings.
+     * @dev A distribution proposal proposal must be in progress.
+     * @dev Only one proposal per DAO can be executed at time.
+     * @dev Only active members can reveice funds.
+     * @dev Only proposals that passed the voting can be set to In Progress status.
+     * @param dao The dao address.
+     * @param proposalId The distribution proposal id.
+     */
+    function processProposal(DaoRegistry dao, bytes32 proposalId)
+        external
+        override
+        reentrancyGuard(dao)
+    {
+        dao.processProposal(proposalId);
+
+        // Checks if the proposal exists or is not in progress yet.
+        Distribution storage distribution =
+            distributions[address(dao)][proposalId];
+        require(
+            distribution.status == DistributionStatus.NOT_STARTED,
+            "proposal already completed or in progress"
+        );
+
+        // Checks if there is an ongoing proposal, only one proposal can be executed at time.
+        bytes32 ongoingProposalId = ongoingDistributions[address(dao)];
+        require(
+            ongoingProposalId == bytes32(0) ||
+                distributions[address(dao)][ongoingProposalId].status !=
+                DistributionStatus.IN_PROGRESS,
+            "another proposal already in progress"
+        );
+
+        // Checks if the proposal has passed.
+        IVoting votingContract = IVoting(dao.votingAdapter(proposalId));
+        require(address(votingContract) != address(0), "adapter not found");
+
+        IVoting.VotingState voteResult =
+            votingContract.voteResult(dao, proposalId);
+        if (voteResult == IVoting.VotingState.PASS) {
+            distribution.status = DistributionStatus.IN_PROGRESS;
+            distribution.blockNumber = block.number;
+            ongoingDistributions[address(dao)] = proposalId;
+
+            BankExtension bank = BankExtension(dao.getExtensionAddress(BANK));
+            uint256 balance = bank.balanceOf(GUILD, distribution.token);
+            require(
+                balance - distribution.amount >= 0,
+                "not enough funds for the given token"
+            );
+
+            bank.internalTransfer(
+                GUILD,
+                ESCROW,
+                distribution.token,
+                distribution.amount
+            );
+        } else if (
+            voteResult == IVoting.VotingState.NOT_PASS ||
+            voteResult == IVoting.VotingState.TIE
+        ) {
+            distribution.status = DistributionStatus.FAILED;
+        } else {
+            revert("proposal has not been voted on");
+        }
+    }
+
+    /**
+     * @notice Transfers the funds from the Guild account to the member's internal accounts.
+     * @notice The amount of funds is caculated using the historical number of units of each member.
+     * @dev A distribution proposal must be in progress.
+     * @dev Only proposals that have passed the voting can be completed.
+     * @dev Only active members can receive funds.
+     * @param dao The dao address.
+     * @param toIndex The index to control the cached for-loop.
+     */
+    function distribute(DaoRegistry dao, uint256 toIndex)
+        external
+        override
+        reentrancyGuard(dao)
+    {
+        // Checks if the proposal does not exist or is not completed yet
+        bytes32 ongoingProposalId = ongoingDistributions[address(dao)];
+        Distribution storage distribution =
+            distributions[address(dao)][ongoingProposalId];
+        uint256 blockNumber = distribution.blockNumber;
+        require(
+            distribution.status == DistributionStatus.IN_PROGRESS,
+            "distribution completed or does not exist"
+        );
+
+        // Check if the given index was already processed
+        uint256 currentIndex = distribution.currentIndex;
+        require(currentIndex <= toIndex, "toIndex too low");
+
+        address token = distribution.token;
+        uint256 amount = distribution.amount;
+
+        // Get the total number of units when the proposal was processed.
+        BankExtension bank = BankExtension(dao.getExtensionAddress(BANK));
+
+        address unitHolderAddr = distribution.unitHolderAddr;
+        if (unitHolderAddr != address(0x0)) {
+            _distributeOne(bank, unitHolderAddr, blockNumber, token, amount);
+            distribution.status = DistributionStatus.DONE;
+            emit Distributed(token, amount, unitHolderAddr);
+        } else {
+            // Set the max index supported which is based on the number of members
+            uint256 nbMembers = dao.getNbMembers();
+            uint256 maxIndex = toIndex;
+            if (maxIndex > nbMembers) {
+                maxIndex = nbMembers;
+            }
+
+            _distributeAll(
+                dao,
+                bank,
+                currentIndex,
+                maxIndex,
+                blockNumber,
+                token,
+                amount
+            );
+
+            distribution.currentIndex = maxIndex;
+            if (maxIndex == nbMembers) {
+                distribution.status = DistributionStatus.DONE;
+                emit Distributed(token, amount, unitHolderAddr);
+            }
+        }
+    }
+
+    /**
+     * @notice Updates the holder account with the amount based on the token parameter.
+     * @notice It is an internal transfer only that happens in the Bank extension.
+     */
+    function _distributeOne(
+        BankExtension bank,
+        address unitHolderAddr,
+        uint256 blockNumber,
+        address token,
+        uint256 amount
+    ) internal {
+        uint256 memberUnits =
+            bank.getPriorAmount(unitHolderAddr, UNITS, blockNumber);
+        require(memberUnits != 0, "not enough units");
+        // Distributes the funds to 1 unit holder only
+        bank.internalTransfer(ESCROW, unitHolderAddr, token, amount);
+    }
+
+    /**
+     * @notice Updates all the holder accounts with the amount based on the token parameter.
+     * @notice It is an internal transfer only that happens in the Bank extension.
+     */
+    function _distributeAll(
+        DaoRegistry dao,
+        BankExtension bank,
+        uint256 currentIndex,
+        uint256 maxIndex,
+        uint256 blockNumber,
+        address token,
+        uint256 amount
+    ) internal {
+        uint256 totalUnits = bank.getPriorAmount(TOTAL, UNITS, blockNumber);
+        // Distributes the funds to all unit holders of the DAO and ignores non-active members.
+        for (uint256 i = currentIndex; i < maxIndex; i++) {
+            address memberAddr = dao.getMemberAddress(i);
+            uint256 memberUnits =
+                bank.getPriorAmount(memberAddr, UNITS, blockNumber);
+            if (memberUnits > 0) {
+                uint256 amountToDistribute =
+                    FairShareHelper.calc(amount, memberUnits, totalUnits);
+
+                if (amountToDistribute > 0) {
+                    bank.internalTransfer(
+                        ESCROW,
+                        memberAddr,
+                        token,
+                        amountToDistribute
+                    );
+                }
+            }
+        }
+    }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,156 @@
+const {
+  toBN,
+  toWei,
+  ETH_TOKEN,
+  maximumChunks,
+  unitPrice,
+  numberOfUnits,
+} = require("../utils/ContractUtil.js");
+
+const { deployDao, getNetworkDetails } = require("../utils/DeploymentUtil.js");
+
+const truffleImports = require("../utils/TruffleUtil.js");
+
+require("dotenv").config();
+
+module.exports = async (deployer, network, accounts) => {
+  let res;
+  const deployFunction = truffleImports.deployFunctionFactory(deployer);
+  if (network === "ganache") {
+    res = await deployGanacheDao(deployFunction, network, accounts);
+  } else if (network === "rinkeby") {
+    res = await deployRinkebyDao(deployFunction, network);
+  } else if (network === "test" || network === "coverage") {
+    res = await deployTestDao(deployFunction, network, accounts);
+  }
+  let { dao, extensions, testContracts } = res;
+  if (dao) {
+    await dao.finalizeDao();
+    console.log("************************");
+    console.log(`DaoRegistry: ${dao.address}`);
+    console.log(
+      `Multicall: ${
+        testContracts.multicall ? testContracts.multicall.address : ""
+      }`
+    );
+    console.log(`BankExtension: ${extensions.bank.address}`);
+    console.log(
+      `NFTExtension: ${extensions.nft ? extensions.nft.address : ""}`
+    );
+    console.log(
+      `ERC20Extension: ${
+        extensions.erc20Ext ? extensions.erc20Ext.address : ""
+      }`
+    );
+    console.log("************************");
+  } else {
+    console.log("************************");
+    console.log("no migration for network " + network);
+    console.log("************************");
+  }
+};
+
+async function deployTestDao(deployFunction, network, accounts) {
+  if (!process.env.DAO_NAME) throw Error("Missing env var: DAO_NAME");
+  if (!process.env.ERC20_TOKEN_NAME)
+    throw Error("Missing env var: ERC20_TOKEN_NAME");
+  if (!process.env.ERC20_TOKEN_SYMBOL)
+    throw Error("Missing env var: ERC20_TOKEN_SYMBOL");
+  if (!process.env.ERC20_TOKEN_DECIMALS)
+    throw Error("Missing env var: ERC20_TOKEN_DECIMALS");
+
+  return await deployDao({
+    ...truffleImports,
+    deployFunction,
+    unitPrice: unitPrice,
+    nbUnits: numberOfUnits,
+    tokenAddr: ETH_TOKEN,
+    erc20TokenName: process.env.ERC20_TOKEN_NAME,
+    erc20TokenSymbol: process.env.ERC20_TOKEN_SYMBOL,
+    erc20TokenDecimals: process.env.ERC20_TOKEN_DECIMALS,
+    maxChunks: maximumChunks,
+    votingPeriod: 10, // 10 secs
+    gracePeriod: 1, // 1 sec
+    offchainVoting: true,
+    chainId: getNetworkDetails(network).chainId,
+    deployTestTokens: false,
+    finalize: false,
+    maxExternalTokens: 100,
+    couponCreatorAddress: "0x7D8cad0bbD68deb352C33e80fccd4D8e88b4aBb8",
+    offchainAdmin: "0xedC10CFA90A135C41538325DD57FDB4c7b88faf7",
+    daoName: process.env.DAO_NAME,
+    owner: accounts[0],
+  });
+}
+
+async function deployRinkebyDao(deployFunction, network) {
+  if (!process.env.DAO_NAME) throw Error("Missing env var: DAO_NAME");
+  if (!process.env.DAO_OWNER_ADDR)
+    throw Error("Missing env var: DAO_OWNER_ADDR");
+  if (!process.env.ERC20_TOKEN_NAME)
+    throw Error("Missing env var: ERC20_TOKEN_NAME");
+  if (!process.env.ERC20_TOKEN_SYMBOL)
+    throw Error("Missing env var: ERC20_TOKEN_SYMBOL");
+  if (!process.env.ERC20_TOKEN_DECIMALS)
+    throw Error("Missing env var: ERC20_TOKEN_DECIMALS");
+
+  return await deployDao({
+    ...truffleImports,
+    deployFunction,
+    unitPrice: toBN(toWei("100", "finney")),
+    nbUnits: toBN("100000"),
+    tokenAddr: ETH_TOKEN,
+    erc20TokenName: process.env.ERC20_TOKEN_NAME,
+    erc20TokenSymbol: process.env.ERC20_TOKEN_SYMBOL,
+    erc20TokenDecimals: process.env.ERC20_TOKEN_DECIMALS,
+    maxChunks: toBN("100000"),
+    votingPeriod: 600, // 600 secs = 10 mins
+    gracePeriod: 600, // 600 secs = 10 mins
+    offchainVoting: true,
+    chainId: getNetworkDetails(network).chainId,
+    deployTestTokens: true,
+    finalize: false,
+    maxExternalTokens: 100,
+    couponCreatorAddress: process.env.COUPON_CREATOR_ADDR
+      ? process.env.COUPON_CREATOR_ADDR
+      : process.env.DAO_OWNER_ADDR,
+    daoName: process.env.DAO_NAME,
+    owner: process.env.DAO_OWNER_ADDR,
+    offchainAdmin: "0xedC10CFA90A135C41538325DD57FDB4c7b88faf7",
+  });
+}
+
+async function deployGanacheDao(deployFunction, network, accounts) {
+  if (!process.env.DAO_NAME) throw Error("Missing env var: DAO_NAME");
+  if (!process.env.ERC20_TOKEN_NAME)
+    throw Error("Missing env var: ERC20_TOKEN_NAME");
+  if (!process.env.ERC20_TOKEN_SYMBOL)
+    throw Error("Missing env var: ERC20_TOKEN_SYMBOL");
+  if (!process.env.ERC20_TOKEN_DECIMALS)
+    throw Error("Missing env var: ERC20_TOKEN_DECIMALS");
+
+  return await deployDao({
+    ...truffleImports,
+    deployFunction,
+    unitPrice: toBN(toWei("100", "finney")),
+    nbUnits: toBN("100000"),
+    tokenAddr: ETH_TOKEN,
+    erc20TokenName: process.env.ERC20_TOKEN_NAME,
+    erc20TokenSymbol: process.env.ERC20_TOKEN_SYMBOL,
+    erc20TokenDecimals: process.env.ERC20_TOKEN_DECIMALS,
+    maxChunks: toBN("100000"),
+    votingPeriod: 120, // 120 secs = 2 mins
+    gracePeriod: 60, // 60 secs = 1 min
+    offchainVoting: true,
+    chainId: getNetworkDetails(network).chainId,
+    deployTestTokens: true,
+    finalize: false,
+    maxExternalTokens: 100,
+    couponCreatorAddress: process.env.COUPON_CREATOR_ADDR
+      ? process.env.COUPON_CREATOR_ADDR
+      : accounts[0],
+    daoName: process.env.DAO_NAME,
+    owner: accounts[0],
+    offchainAdmin: "0xedC10CFA90A135C41538325DD57FDB4c7b88faf7",
+  });
+}
