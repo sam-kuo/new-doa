bb3f3c3f11b547aa03ffdc08bb2e9a7613a95906
s <65716060+sam-kuo@users.noreply.github.com>
2022-05-26 21:09:23 +0800

    Initialize mainnet-deploy-2

---
+++ truffle-config.js
@@ -0,0 +1,82 @@
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * truffleframework.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura accounts
+ * are available for free at: infura.io/register.
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
+require("dotenv").config();
+require("solidity-coverage");
+
+module.exports = {
+  networks: {
+    ganache: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: "1337", // Any network (default: none)
+    },
+    rinkeby: {
+      provider: function () {
+        let infuraKey = process.env.INFURA_KEY;
+        let HDWalletProvider = require("@truffle/hdwallet-provider");
+        let mnemonic = process.env.TRUFFLE_MNEMONIC;
+        let infuraUrl = "wss://rinkeby.infura.io/ws/v3/" + infuraKey;
+        return new HDWalletProvider(mnemonic, infuraUrl);
+      },
+      network_id: 4,
+      gasPrice: 10000000000,
+      skipDryRun: true,
+    },
+    mainnet: {
+      provider: function () {
+        let infuraKey = process.env.INFURA_KEY;
+        let HDWalletProvider = require("@truffle/hdwallet-provider");
+        let mnemonic = process.env.TRUFFLE_MNEMONIC;
+        let infuraUrl = "wss://mainnet.infura.io/ws/v3/" + infuraKey;
+        return new HDWalletProvider(mnemonic, infuraUrl);
+      },
+      network_id: 1,
+      skipDryRun: true,
+    },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+    },
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.0", // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 10000,
+        },
+        //  evmVersion: "byzantium"
+      },
+    },
+  },
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY, // Obtain one at https://etherscan.io/myapikey
+  },
+  plugins: ["solidity-coverage", "truffle-plugin-verify"],
+};
---
+++ utils/ContractUtil.js
@@ -0,0 +1,126 @@
+// Whole-script strict mode syntax
+"use strict";
+
+/**
+MIT License
+
+Copyright (c) 2020 Openlaw
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+const Web3Utils = require("web3-utils");
+const sha3 = Web3Utils.sha3;
+const toBN = Web3Utils.toBN;
+const toWei = Web3Utils.toWei;
+const fromUtf8 = Web3Utils.fromUtf8;
+const hexToBytes = Web3Utils.hexToBytes;
+const toAscii = Web3Utils.toAscii;
+const fromAscii = Web3Utils.fromAscii;
+const toUtf8 = Web3Utils.toUtf8;
+
+const GUILD = "0x000000000000000000000000000000000000dead";
+const TOTAL = "0x000000000000000000000000000000000000babe";
+const ESCROW = "0x0000000000000000000000000000000000004bec";
+const MEMBER_COUNT = "0x00000000000000000000000000000000DECAFBAD";
+const UNITS = "0x00000000000000000000000000000000000FF1CE";
+const LOOT = "0x00000000000000000000000000000000B105F00D";
+const ETH_TOKEN = "0x0000000000000000000000000000000000000000";
+const DAI_TOKEN = "0x95b58a6bff3d14b7db2f5cb5f0ad413dc2940658";
+
+const numberOfUnits = toBN("1000000000000000");
+const unitPrice = toBN(toWei("120", "finney"));
+const remaining = unitPrice.sub(toBN("50000000000000"));
+const maximumChunks = toBN("11");
+
+const contracts = {
+  // Test Util Contracts
+  OLToken: "./test/OLToken",
+  TestToken1: "./test/TestToken1",
+  TestToken2: "./test/TestToken2",
+  TestFairShareCalc: "./test/TestFairShareCalc",
+  PixelNFT: "./test/PixelNFT",
+
+  // DAO Contracts
+  DaoFactory: "./core/DaoFactory",
+  DaoRegistry: "./core/DaoRegistry",
+  NFTCollectionFactory: "./extensions/NFTCollectionFactory",
+  BankFactory: "./extensions/bank/BankFactory",
+  ERC20TokenExtensionFactory:
+    "./extensions/token/erc20/ERC20TokenExtensionFactory",
+  Multicall: "./util/Multicall",
+
+  // Extensions
+  NFTExtension: "./extensions/nft/NFTExtension",
+  BankExtension: "./extensions/bank/BankExtension",
+  ERC20Extension: "./extensions/token/erc20/ERC20Extension",
+
+  // Config Adapters
+  DaoRegistryAdapterContract: "./adapters/DaoRegistryAdapterContract",
+  BankAdapterContract: "./adapters/BankAdapterContract",
+  NFTAdapterContract: "./adapters/NFTAdapterContract",
+  ConfigurationContract: "./adapters/ConfigurationContract",
+  ManagingContract: "./adapters/ManagingContract",
+
+  // Voting Adapters
+  VotingContract: "./adapters/VotingContract",
+  SnapshotProposalContract: "./adapters/voting/SnapshotProposalContract",
+  OffchainVotingContract: "./adapters/voting/OffchainVotingContract",
+  KickBadReporterAdapter: "./adapters/voting/KickBadReporterAdapter",
+  BatchVotingContract: "./adapters/voting/BatchVotingContract",
+
+  // Withdraw Adapters
+  RagequitContract: "./adapters/RagequitContract",
+  GuildKickContract: "./adapters/GuildKickContract",
+  DistributeContract: "./adapters/DistributeContract",
+
+  // Funding/Onboarding Adapters
+  FinancingContract: "./adapters/FinancingContract",
+  OnboardingContract: "./adapters/OnboardingContract",
+  CouponOnboardingContract: "./adapters/CouponOnboardingContract",
+  TributeContract: "./adapters/TributeContract",
+  TributeNFTContract: "./adapters/TributeNFTContract",
+
+  // Utils
+  DaoArtifacts: "./utils/DaoArtifacts",
+};
+
+module.exports = {
+  contracts,
+  sha3,
+  toBN,
+  toWei,
+  hexToBytes,
+  fromUtf8,
+  toAscii,
+  fromAscii,
+  toUtf8,
+  maximumChunks,
+  numberOfUnits,
+  unitPrice,
+  remaining,
+  GUILD,
+  TOTAL,
+  ESCROW,
+  DAI_TOKEN,
+  UNITS,
+  MEMBER_COUNT,
+  LOOT,
+  ETH_TOKEN,
+};
