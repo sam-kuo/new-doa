a0243ea5ae737b8f06a94a5a5a3b440039cfc48e
s <65716060+sam-kuo@users.noreply.github.com>
2022-05-26 21:09:47 +0800

    Initialize release-v2.2.2

---
+++ scripts/coverage.ts
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+const { runCoverage } = require("@openzeppelin/test-environment");
+
+const skipFiles = [
+  "Migrations.sol",
+  "adapters/interfaces/IConfiguration.sol",
+  "adapters/interfaces/IDistribute.sol",
+  "adapters/interfaces/IFinancing.sol",
+  "adapters/interfaces/IGuildKick.sol",
+  "adapters/interfaces/IManaging.sol",
+  "adapters/interfaces/IOnboarding.sol",
+  "adapters/interfaces/IRagequit.sol",
+  "adapters/interfaces/ISignatures.sol",
+  "adapters/interfaces/IVoting.sol",
+  "extensions/IExtension.sol",
+  "test/MockDao.sol",
+  "test/ERC1155TestToken.sol",
+  "test/ERC20Minter.sol",
+  "test/OLToken.sol",
+  "test/PixelNFT.sol",
+  "test/ProxToken.sol",
+  "test/TestFairShareCalc.sol",
+  "test/TestToken1.sol",
+  "test/TestToken2.sol",
+  "utils/Multicall.sol",
+];
+
+const main = async () => {
+  await runCoverage(
+    skipFiles,
+    "npm run compile",
+    "./node_modules/.bin/mocha --timeout 2000000 --recursive --exit".split(" ")
+  );
+};
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
