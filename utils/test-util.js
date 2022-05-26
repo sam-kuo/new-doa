5748abf06fd73175fa6f1a8737ce467724f94fe0
s <65716060+sam-kuo@users.noreply.github.com>
2022-05-26 21:10:01 +0800

    Initialize new-snapshot

---
+++ utils/test-util.js
@@ -0,0 +1,172 @@
+// Whole-script strict mode syntax
+"use strict";
+
+const { toBN, UNITS } = require("./contract-util");
+
+const { expect, advanceTime, web3 } = require("./oz-util");
+
+const checkLastEvent = async (dao, testObject) => {
+  let pastEvents = await dao.getPastEvents();
+  let returnValues = pastEvents[0].returnValues;
+
+  Object.keys(testObject).forEach((key) =>
+    expect(testObject[key], "value mismatch for key " + key).equal(
+      returnValues[key]
+    )
+  );
+};
+
+const checkBalance = async (bank, address, token, expectedBalance) => {
+  const balance = await bank.balanceOf(address, token);
+
+  expect(balance.toString()).equal(expectedBalance.toString());
+};
+
+const checkSignature = async (
+  signatureExtension,
+  permissionHash,
+  signature,
+  magicValue
+) => {
+  const returnedValue = await signatureExtension.isValidSignature(
+    permissionHash,
+    signature
+  );
+
+  expect(returnedValue).equal(magicValue);
+};
+
+const encodeDaoInfo = (daoAddress) =>
+  web3.eth.abi.encodeParameter(
+    {
+      DaoInfo: {
+        dao: "address",
+      },
+    },
+    {
+      dao: daoAddress,
+    }
+  );
+
+const isMember = async (bank, member) => {
+  const units = await bank.balanceOf(member, UNITS);
+
+  return units > toBN("0");
+};
+
+const submitNewMemberProposal = async (
+  proposalId,
+  member,
+  onboarding,
+  dao,
+  newMember,
+  unitPrice,
+  token,
+  desiredUnits = toBN(10)
+) => {
+  await onboarding.submitProposal(
+    dao.address,
+    proposalId,
+    newMember,
+    token,
+    unitPrice.mul(desiredUnits),
+    [],
+    {
+      from: member,
+      gasPrice: toBN("0"),
+    }
+  );
+};
+
+const onboardingNewMember = async (
+  proposalId,
+  dao,
+  onboarding,
+  voting,
+  newMember,
+  sponsor,
+  unitPrice,
+  token,
+  desiredUnits = toBN(10)
+) => {
+  await submitNewMemberProposal(
+    proposalId,
+    sponsor,
+    onboarding,
+    dao,
+    newMember,
+    unitPrice,
+    token,
+    desiredUnits
+  );
+
+  //vote and process it
+  await voting.submitVote(dao.address, proposalId, 1, {
+    from: sponsor,
+    gasPrice: toBN("0"),
+  });
+  await advanceTime(10000);
+
+  await onboarding.processProposal(dao.address, proposalId, {
+    from: sponsor,
+    value: unitPrice.mul(desiredUnits),
+    gasPrice: toBN("0"),
+  });
+};
+
+const guildKickProposal = async (
+  dao,
+  guildkickContract,
+  memberToKick,
+  sender,
+  proposalId
+) => {
+  await guildkickContract.submitProposal(
+    dao.address,
+    proposalId,
+    memberToKick,
+    [],
+    {
+      from: sender,
+      gasPrice: toBN("0"),
+    }
+  );
+};
+
+const submitConfigProposal = async (
+  dao,
+  proposalId,
+  sender,
+  configuration,
+  voting,
+  configs
+) => {
+  //Submit a new configuration proposal
+  await configuration.submitProposal(dao.address, proposalId, configs, [], {
+    from: sender,
+    gasPrice: toBN("0"),
+  });
+
+  await voting.submitVote(dao.address, proposalId, 1, {
+    from: sender,
+    gasPrice: toBN("0"),
+  });
+
+  await advanceTime(10000);
+  await configuration.processProposal(dao.address, proposalId, {
+    from: sender,
+    gasPrice: toBN("0"),
+  });
+};
+
+module.exports = {
+  checkLastEvent,
+  checkBalance,
+  checkSignature,
+  submitNewMemberProposal,
+  onboardingNewMember,
+  guildKickProposal,
+  submitConfigProposal,
+  isMember,
+  encodeDaoInfo,
+};
