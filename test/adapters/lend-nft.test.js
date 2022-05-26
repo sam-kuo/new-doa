b7c0f27f5b14973adde94c6c4ab0d3a914f646b5
s <65716060+sam-kuo@users.noreply.github.com>
2022-05-26 21:10:19 +0800

    Initialize feat/defender+hardhat-deployer

---
+++ test/adapters/lend-nft.test.js
@@ -0,0 +1,207 @@
+// Whole-script strict mode syntax
+"use strict";
+
+/**
+MIT License
+
+Copyright (c) 2020 Openlaw
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+const {
+  toBN,
+  toWei,
+  ERC1155,
+  UNITS,
+  fromAscii,
+} = require("../../utils/contract-util");
+
+const {
+  deployDefaultNFTDao,
+  takeChainSnapshot,
+  revertChainSnapshot,
+  proposalIdGenerator,
+  advanceTime,
+  accounts,
+  encodeProposalData,
+  expectRevert,
+  expect,
+  web3,
+} = require("../../utils/oz-util");
+
+describe("Adapter - LendNFT", () => {
+  const proposalCounter = proposalIdGenerator().generator;
+  const daoOwner = accounts[1];
+
+  const getProposalCounter = () => {
+    return proposalCounter().next().value;
+  };
+
+  before("deploy dao", async () => {
+    const { dao, adapters, extensions, testContracts } =
+      await deployDefaultNFTDao({ owner: daoOwner });
+    this.dao = dao;
+    this.adapters = adapters;
+    this.extensions = extensions;
+    this.testContracts = testContracts;
+    this.snapshotId = await takeChainSnapshot();
+  });
+
+  beforeEach(async () => {
+    await revertChainSnapshot(this.snapshotId);
+    this.snapshotId = await takeChainSnapshot();
+  });
+
+  it("should be able to lend an NFT", async () => {
+    const pixelNFT = this.testContracts.pixelNFT;
+    const erc1155Token = this.testContracts.erc1155TestToken;
+    const nftOwner = accounts[2];
+    const lendNFT = this.adapters.lendNFT;
+    const dao = this.dao;
+    const voting = this.adapters.voting;
+    const nftExtension = this.extensions.erc721Ext;
+    const bank = this.extensions.bankExt;
+    const proposalId = getProposalCounter();
+    const proposalId2 = getProposalCounter();
+
+    //mint 2 NFTs
+    await pixelNFT.mintPixel(nftOwner, 1, 1, { from: daoOwner });
+
+    let pastEvents = await pixelNFT.getPastEvents();
+    const { tokenId } = pastEvents[1].returnValues;
+    const tokenId2 = 10;
+    await erc1155Token.mint(nftOwner, tokenId2, 1, [], { from: daoOwner });
+
+    //create 2 proposals, one for each NFT
+    await lendNFT.submitProposal(
+      dao.address,
+      proposalId,
+      nftOwner,
+      pixelNFT.address,
+      tokenId,
+      10000,
+      10000, // requested units
+      [],
+      { from: daoOwner, gasPrice: toBN("0") }
+    );
+
+    await lendNFT.submitProposal(
+      dao.address,
+      proposalId2,
+      nftOwner,
+      erc1155Token.address,
+      tokenId2,
+      10000,
+      10000, // requested units
+      [],
+      { from: daoOwner, gasPrice: toBN("0") }
+    );
+
+    //vote them in
+
+    await voting.submitVote(dao.address, proposalId, 1, { from: daoOwner });
+    await voting.submitVote(dao.address, proposalId2, 1, { from: daoOwner });
+
+    await advanceTime(10000);
+
+    //approve each NFT to the NFT extension
+    await pixelNFT.methods["safeTransferFrom(address,address,uint256,bytes)"](
+      nftOwner,
+      lendNFT.address,
+      tokenId,
+      encodeProposalData(dao, proposalId),
+      {
+        from: nftOwner,
+      }
+    );
+
+    //we see that we have 10K units
+    let unitBalance = await bank.balanceOf(nftOwner, UNITS);
+    expect(unitBalance.toString()).equal("10000");
+
+    let currentNftOwner = await pixelNFT.ownerOf(tokenId);
+    expect(currentNftOwner).equal(nftExtension.address);
+
+    await advanceTime(100);
+    // after 100 seconds, we take the NFT back
+    await expectRevert(
+      lendNFT.sendNFTBack(dao.address, proposalId),
+      "only the previous owner can withdraw the NFT"
+    );
+    await lendNFT.sendNFTBack(dao.address, proposalId, { from: nftOwner });
+
+    unitBalance = await bank.balanceOf(nftOwner, UNITS);
+    expect(toBN(unitBalance.toString())).to.be.closeTo(toBN("100"), 5);
+
+    await advanceTime(10000);
+    //process the second proposal
+    await erc1155Token.safeTransferFrom(
+      nftOwner,
+      lendNFT.address,
+      tokenId2,
+      1,
+      encodeProposalData(dao, proposalId2),
+      { from: nftOwner }
+    );
+
+    const erc1155ExtAddr = await dao.getExtensionAddress(ERC1155);
+    const balanceOf = await erc1155Token.balanceOf(erc1155ExtAddr, tokenId2);
+    expect(balanceOf.toString()).equal("1");
+
+    unitBalance = await bank.balanceOf(nftOwner, UNITS);
+    expect(toBN(unitBalance.toString())).to.be.closeTo(toBN("10100"), 5);
+
+    await advanceTime(100);
+
+    //after 100 seconds, get the second NFT back
+    await lendNFT.sendNFTBack(dao.address, proposalId2, { from: nftOwner });
+    unitBalance = await bank.balanceOf(nftOwner, UNITS);
+    expect(toBN(unitBalance.toString())).to.be.closeTo(toBN("200"), 5);
+
+    const balance = await erc1155Token.balanceOf(nftOwner, tokenId2);
+    expect(balance.toString()).equal("1");
+  });
+
+  it("should not be possible to send ETH to the adapter via receive function", async () => {
+    const adapter = this.adapters.lendNFT;
+    await expectRevert(
+      web3.eth.sendTransaction({
+        to: adapter.address,
+        from: daoOwner,
+        gasPrice: toBN("0"),
+        value: toWei("1"),
+      }),
+      "revert"
+    );
+  });
+
+  it("should not be possible to send ETH to the adapter via fallback function", async () => {
+    const adapter = this.adapters.lendNFT;
+    await expectRevert(
+      web3.eth.sendTransaction({
+        to: adapter.address,
+        from: daoOwner,
+        gasPrice: toBN("0"),
+        value: toWei("1"),
+        data: fromAscii("should go to fallback func"),
+      }),
+      "revert"
+    );
+  });
+});
---
+++ test/extensions/vesting.test.js
@@ -0,0 +1,245 @@
+// Whole-script strict mode syntax
+"use strict";
+
+/**
+MIT License
+
+Copyright (c) 2020 Openlaw
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+const { UNITS, toBN } = require("../../utils/contract-util");
+
+const {
+  takeChainSnapshot,
+  revertChainSnapshot,
+  deployDefaultDao,
+  advanceTime,
+  accounts,
+  expect,
+  expectRevert,
+} = require("../../utils/oz-util");
+
+describe("Extension - Vesting", () => {
+  const daoOwner = accounts[0];
+
+  before("deploy dao", async () => {
+    const { dao, adapters, extensions, testContracts } = await deployDefaultDao(
+      { owner: daoOwner, finalize: false }
+    );
+    this.dao = dao;
+    this.adapters = adapters;
+    this.extensions = extensions;
+    this.testContracts = testContracts;
+    this.snapshotId = await takeChainSnapshot();
+  });
+
+  beforeEach(async () => {
+    this.snapshotId = await takeChainSnapshot();
+  });
+
+  afterEach(async () => {
+    await revertChainSnapshot(this.snapshotId);
+  });
+
+  it("should be able to create vesting and the blocked amount should change with time", async () => {
+    const vesting = this.extensions.vestingExt;
+    const now = new Date();
+
+    const numberOfDaysToAdd = 6;
+    now.setDate(now.getDate() + numberOfDaysToAdd);
+    let minBalance = await vesting.getMinimumBalance(daoOwner, UNITS);
+    expect(minBalance.toString()).equal("0");
+
+    await vesting.createNewVesting(
+      this.dao.address,
+      daoOwner,
+      UNITS,
+      1000,
+      Math.floor(now.getTime() / 1000),
+      { from: daoOwner }
+    );
+
+    const v = await vesting.vesting(daoOwner, UNITS);
+    const diff = toBN(v.endDate.toString()).sub(toBN(v.startDate.toString()));
+
+    minBalance = await vesting.getMinimumBalance(daoOwner, UNITS);
+    expect(minBalance.toString()).equal("1000");
+
+    const halfWay = diff.div(toBN("2"));
+
+    await advanceTime(halfWay.toNumber());
+
+    minBalance = await vesting.getMinimumBalance(daoOwner, UNITS);
+    expect(toBN(minBalance.toString())).to.be.closeTo(toBN("500"), 5);
+
+    await advanceTime(diff.toNumber());
+
+    minBalance = await vesting.getMinimumBalance(daoOwner, UNITS);
+    expect(minBalance.toString()).equal("0");
+  });
+
+  it("should be able to add multiple vestings", async () => {
+    const vesting = this.extensions.vestingExt;
+    const now = new Date();
+
+    const numberOfDaysToAdd = 6;
+    now.setDate(now.getDate() + numberOfDaysToAdd);
+    let minBalance = await vesting.getMinimumBalance(daoOwner, UNITS);
+    expect(minBalance.toString()).equal("0");
+
+    await vesting.createNewVesting(
+      this.dao.address,
+      daoOwner,
+      UNITS,
+      100,
+      Math.floor(now.getTime() / 1000),
+      { from: daoOwner }
+    );
+
+    let v = await vesting.vesting(daoOwner, UNITS);
+    let diff = toBN(v.endDate.toString()).sub(toBN(v.startDate.toString()));
+
+    minBalance = await vesting.getMinimumBalance(daoOwner, UNITS);
+    expect(minBalance.toString()).equal("100");
+
+    let halfWay = diff.div(toBN("2"));
+
+    await advanceTime(halfWay.toNumber());
+
+    now.setDate(now.getDate() + numberOfDaysToAdd);
+
+    await vesting.createNewVesting(
+      this.dao.address,
+      daoOwner,
+      UNITS,
+      100,
+      Math.floor(now.getTime() / 1000),
+      { from: daoOwner }
+    );
+
+    v = await vesting.vesting(daoOwner, UNITS);
+    diff = toBN(v.endDate.toString()).sub(toBN(v.startDate.toString()));
+    halfWay = diff.div(toBN("2"));
+
+    minBalance = await vesting.getMinimumBalance(daoOwner, UNITS);
+    expect(toBN(minBalance.toString())).to.be.closeTo(toBN("150"), 5);
+
+    await advanceTime(halfWay.toNumber());
+
+    minBalance = await vesting.getMinimumBalance(daoOwner, UNITS);
+    expect(toBN(minBalance.toString())).to.be.closeTo(toBN("75"), 5);
+
+    await advanceTime(diff.toNumber());
+
+    minBalance = await vesting.getMinimumBalance(daoOwner, UNITS);
+    expect(minBalance.toString()).equal("0");
+  });
+
+  it("should be possible to remove vesting", async () => {
+    const vesting = this.extensions.vestingExt;
+    const now = new Date();
+
+    const numberOfDaysToAdd = 6;
+    now.setDate(now.getDate() + numberOfDaysToAdd);
+    let minBalance = await vesting.getMinimumBalance(daoOwner, UNITS);
+    expect(minBalance.toString()).equal("0");
+
+    await vesting.createNewVesting(
+      this.dao.address,
+      daoOwner,
+      UNITS,
+      100,
+      Math.floor(now.getTime() / 1000),
+      { from: daoOwner }
+    );
+
+    let v = await vesting.vesting(daoOwner, UNITS);
+    let diff = toBN(v.endDate.toString()).sub(toBN(v.startDate.toString()));
+
+    minBalance = await vesting.getMinimumBalance(daoOwner, UNITS);
+    expect(minBalance.toString()).equal("100");
+
+    let halfWay = diff.div(toBN("2"));
+
+    await advanceTime(halfWay.toNumber());
+
+    now.setDate(now.getDate() + numberOfDaysToAdd);
+
+    await vesting.createNewVesting(
+      this.dao.address,
+      daoOwner,
+      UNITS,
+      100,
+      Math.floor(now.getTime() / 1000),
+      { from: daoOwner }
+    );
+
+    v = await vesting.vesting(daoOwner, UNITS);
+    diff = toBN(v.endDate.toString()).sub(toBN(v.startDate.toString()));
+    halfWay = diff.div(toBN("2"));
+
+    minBalance = await vesting.getMinimumBalance(daoOwner, UNITS);
+    //to manage rounding error
+    expect(toBN(minBalance.toString())).to.be.closeTo(toBN("150"), 5);
+
+    await advanceTime(halfWay.toNumber());
+
+    minBalance = await vesting.getMinimumBalance(daoOwner, UNITS);
+    expect(toBN(minBalance.toString())).to.be.closeTo(toBN("75"), 5);
+
+    await vesting.removeVesting(this.dao.address, daoOwner, UNITS, 50, {
+      from: daoOwner,
+    });
+
+    minBalance = await vesting.getMinimumBalance(daoOwner, UNITS);
+    expect(toBN(minBalance.toString())).to.be.closeTo(toBN("50"), 5);
+  });
+
+  it("should not be possible to create a new vesting without the ACL permission", async () => {
+    // Finalize the DAO to be able to check the extension permissions
+    await this.dao.finalizeDao({ from: daoOwner });
+    const vesting = this.extensions.vestingExt;
+    const now = new Date();
+    await expectRevert(
+      vesting.createNewVesting(
+        this.dao.address,
+        daoOwner,
+        UNITS,
+        100,
+        Math.floor(now.getTime() / 1000),
+        { from: daoOwner }
+      ),
+      "vestingExt::accessDenied"
+    );
+  });
+
+  it("should not be possible to removeVesting a vesting schedule the without ACL permission", async () => {
+    // Finalize the DAO to be able to check the extension permissions
+    await this.dao.finalizeDao({ from: daoOwner });
+    const vesting = this.extensions.vestingExt;
+    await expectRevert(
+      vesting.removeVesting(this.dao.address, daoOwner, UNITS, 100, {
+        from: daoOwner,
+      }),
+      "vestingExt::accessDenied"
+    );
+  });
+});
